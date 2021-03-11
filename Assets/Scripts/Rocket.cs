6cbe8ffd907a367f524accb96c42b571b5fb0635
Daniel du Plessis <daniedup@cybersmart.co.za>
2021-03-11 12:41:24 +0200

    9_PB_CU2

--- Assets/Scripts/Rocket.cs
+++ Assets/Scripts/Rocket.cs
@@ -2,12 +2,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Rocket : MonoBehaviour
 {
     Rigidbody rigidBody;
     AudioSource audioSource;
-
+    [SerializeField] float rcsThrust = 100f;
+    [SerializeField] float mainThrust = 50f;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,39 +20,67 @@ public class Rocket : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        ProcessInput();
+        Thrust();
+        Rotate();
     }
 
-    private void ProcessInput()
+    private void OnCollisionEnter(Collision collision)
     {
+        switch (collision.gameObject.tag)
+        {
+            case "Friendly":
+                {
+                    print("Friend");
+                    break;
+                }
+
+            case "Finish":
+                {
+                    print("Game Finish");
+                    SceneManager.LoadScene(1);
+                    break;
+                }
+
+            default:
+                {
+                    print("You Died");
+                    SceneManager.LoadScene(0);
+                    break;
+                }
+
+        }
+    }
+    private void Rotate()
+    {
+        rigidBody.freezeRotation = true; //take manual control of rotation
+        float rotationSpeed = rcsThrust * Time.deltaTime;
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(Vector3.back * rotationSpeed);
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(Vector3.forward * rotationSpeed);
+        }
+        rigidBody.freezeRotation = false; //resume physics control of rotation
+    }
+
+    private void Thrust()
+    {
+        rigidBody.freezeRotation = true; //take manual control of rotation
         if (Input.GetKey(KeyCode.Space)) //can thrust while rotating
         {
-            if(!audioSource.isPlaying)
+            if (!audioSource.isPlaying)
             {
                 audioSource.Play();
             }
-            rigidBody.AddRelativeForce(Vector3.up );
+            rigidBody.AddRelativeForce(Vector3.up * mainThrust);
 
         }
         else
         {
             audioSource.Stop();
         }
-        if (Input.GetKey(KeyCode.D))
-        {
-            transform.Rotate(Vector3.back); 
-        }
-        else if (Input.GetKey(KeyCode.A))
-        {
-            transform.Rotate(Vector3.forward);
-        }
-        //if (Input.GetKeyDown(KeyCode.Space))
-        //{
-            audioSource.Play();
-        //}
-        //if (Input.GetKeyUp(KeyCode.Space))
-        //{
-            audioSource.Stop();
-        //}
+        rigidBody.freezeRotation = false; //resume physics control of rotatio
     }
 }
